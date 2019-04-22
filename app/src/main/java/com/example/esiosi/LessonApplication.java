481d05ee4933229eadf70ef6cee3572f0ca8c975
Dennis Chen <z5118055@ad.unsw.edu.au>
2019-04-22 16:45:33 +1000

    Youtube Api added

--- app/src/main/java/com/example/esiosi/LessonApplication.java
+++ app/src/main/java/com/example/esiosi/LessonApplication.java
@@ -1,13 +1,62 @@
 package com.example.esiosi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
-public class LessonApplication extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class LessonApplication extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+
+    static final String GOOGLE_API_KEY = "AIzaSyAoACxRsjrQ1gX5V5hwFHNlnsGYSCfNYms";
+    static final String YOUTUBE_VIDEO_ID = "MQUi9NMMO5Y";
+
+    Toolbar toolbar;
+
+    TextView textView;
+
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_application);
+
+
+        textView = (TextView)findViewById(R.id.textView);
+
+        textView.setText("Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test ");
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Application Layer");
+
+
     }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+        youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
+
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        }
+    }
+
 }

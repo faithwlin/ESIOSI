66b33fd2acb65642c6b01937115dc3ae217c7672
dennisc111 <47768505+dennisc111@users.noreply.github.com>
2019-04-23 22:34:46 +1000

    Quiz complete update

--- app/src/main/java/com/example/esiosi/QuizComplete.java
+++ app/src/main/java/com/example/esiosi/QuizComplete.java
@@ -1,12 +1,16 @@
 package com.example.esiosi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class QuizComplete extends AppCompatActivity {
 
     private TextView score;
+    private ImageView finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,16 @@ public class QuizComplete extends AppCompatActivity {
 
         int mScore = bundle.getInt("Scores");
         score.setText(mScore + " / 10");
+
+        finish = (ImageView)findViewById(R.id.finishButton);
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(QuizComplete.this, QuizHome.class);
+                startActivity(i);
+
+            }
+        });
 
 
 
